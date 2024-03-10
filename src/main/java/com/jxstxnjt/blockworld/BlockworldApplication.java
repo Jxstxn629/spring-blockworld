ea81562e0fb09e06fd26340c1d8535d2a32c47c3
Justin Ng <justin@Justins-MacBook-Air.local>
2024-03-11 04:59:30 +0800

    init

---
+++ src/main/java/com/jxstxnjt/blockworld/BlockworldApplication.java
@@ -0,0 +1,13 @@
+package com.jxstxnjt.blockworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockworldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jxstxnjt/blockworld/BlockworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jxstxnjt.blockworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
