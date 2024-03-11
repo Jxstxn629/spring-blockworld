47a9a31d884a38648d152025761ce996ce880963
Justin Ng <justin@Justins-MacBook-Air.local>
2024-03-11 13:27:20 +0800

    dockerize application

--- src/main/java/com/jxstxnjt/blockworld/BlockworldApplication.java
+++ src/main/java/com/jxstxnjt/blockworld/BlockworldApplication.java
@@ -2,6 +2,7 @@ package com.jxstxnjt.blockworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class BlockworldApplication {
@@ -10,4 +11,8 @@ public class BlockworldApplication {
 		SpringApplication.run(BlockworldApplication.class, args);
 	}
 
+	@GetMapping("/health-check")
+	public String healthCheck() {
+		return "Healthy!";
+	}
 }
--- src/test/java/com/jxstxnjt/blockworld/BlockworldApplicationTests.java
+++ src/test/java/com/jxstxnjt/blockworld/BlockworldApplicationTests.java
@@ -3,7 +3,7 @@ package com.jxstxnjt.blockworld;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = BlockworldApplicationTests.class)
 class BlockworldApplicationTests {
 
 	@Test
